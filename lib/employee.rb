392a57c0b49a5e51b770d97d0e0f4bfe31ff81a8
togmund <togmund@gmail.com>
2019-12-10 23:47:50 +0000

    have most validations set up, testing the chomp

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,7 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates  :store,       presence: true
+  validates  :first_name,  presence: true
+  validates  :last_name,   presence: true
+  validates  :hourly_rate, :numericality => { :only_integer => true }, :inclusion => { :in => 40..200 }
 end
